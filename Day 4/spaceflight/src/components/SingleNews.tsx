a5488c4b2a7c8680cb670404aa0415149bce9bbc
Carmy93 <carmygargiulo45@gmail.com>
2024-09-05 15:37:55 +0200

    update

--- Day 4/spaceflight/src/components/SingleNews.tsx
+++ Day 4/spaceflight/src/components/SingleNews.tsx
@@ -1,6 +1,6 @@
 import { Card, Button } from "react-bootstrap"
 import { Welcome } from "../interfaces/interface"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 interface SingleNewsProps {
     single: Welcome
 }
@@ -10,15 +10,36 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 
 function SingleNews({single}: SingleNewsProps) {
     const navigate = useNavigate()
+    const location = useLocation()
     const date = new Date(single.published_at)
     return (
         <Card className="text-center">
-            <Card.Header>{single.news_site}</Card.Header>
-            <Card.Img variant="top" src={single.image_url}/>
+            {
+                location.pathname === `/details/${single.id}` && (
+                    <>
+                        <Card.Header>{single.news_site}</Card.Header>
+                        <Card.Img variant="top" src={single.image_url}/>
+                    </>
+                )
+            }
+            
             <Card.Body>
                 <Card.Title><a href={single.url} title="a">{single.title}</a></Card.Title>
-                <Card.Text>{single.summary}                    
-                </Card.Text>
+                {
+                    location.pathname !== `/details/${single.id}` ? (
+                        single.summary.length > 90 ? (
+                            <Card.Text>{single.summary.slice(0, single.summary.indexOf(' ', 90))}<span className="fw-bold">...See more </span>                   
+                            </Card.Text>
+                        ) : (
+                            <Card.Text>{single.summary}                   
+                            </Card.Text>
+                        )
+                        
+                    ) : (
+                        <Card.Text>{single.summary}</Card.Text>
+                    )
+                }
+               
                 <Button variant="primary" onClick={()=>navigate(`/details/${single.id}`)}>Check the news</Button>
             </Card.Body>
             <Card.Footer className="text-muted">{`${days[date.getDay()]}  ${date.getDate()} ${months[date.getMonth()]} - ${date.getHours()}:${date.getMinutes() === 0 ? '00': date.getMinutes()}`}</Card.Footer>
