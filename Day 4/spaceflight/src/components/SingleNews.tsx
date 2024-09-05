4e5ca3343fe896b2fa44d0c65b365bfc2c01640a
Carmy93 <carmygargiulo45@gmail.com>
2024-09-05 15:19:55 +0200

    update

--- Day 4/spaceflight/src/components/SingleNews.tsx
+++ Day 4/spaceflight/src/components/SingleNews.tsx
@@ -1,5 +1,6 @@
 import { Card, Button } from "react-bootstrap"
 import { Welcome } from "../interfaces/interface"
+import { useNavigate } from "react-router-dom"
 interface SingleNewsProps {
     single: Welcome
 }
@@ -8,17 +9,17 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Dicember']
 
 function SingleNews({single}: SingleNewsProps) {
+    const navigate = useNavigate()
     const date = new Date(single.published_at)
-    console.log(date)
     return (
         <Card className="text-center">
             <Card.Header>{single.news_site}</Card.Header>
-            <Card.Img variant="top" src={single.image_url} /* height={600} */ />
+            <Card.Img variant="top" src={single.image_url}/>
             <Card.Body>
                 <Card.Title><a href={single.url} title="a">{single.title}</a></Card.Title>
                 <Card.Text>{single.summary}                    
                 </Card.Text>
-                <Button variant="primary">Check the news</Button>
+                <Button variant="primary" onClick={()=>navigate(`/details/${single.id}`)}>Check the news</Button>
             </Card.Body>
             <Card.Footer className="text-muted">{`${days[date.getDay()]}  ${date.getDate()} ${months[date.getMonth()]} - ${date.getHours()}:${date.getMinutes() === 0 ? '00': date.getMinutes()}`}</Card.Footer>
         </Card>
