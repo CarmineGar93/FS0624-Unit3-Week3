e890ed7c469786e9343bd5034039583fe2a570ff
Carmy93 <carmygargiulo45@gmail.com>
2024-09-05 16:58:41 +0200

    loading

--- Day 4/spaceflight/src/components/MyNews.tsx
+++ Day 4/spaceflight/src/components/MyNews.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Spinner } from "react-bootstrap"
 import { Welcome } from '../interfaces/interface'
 import SingleNews from "./SingleNews"
 interface MyNewsProps {
@@ -9,8 +9,9 @@ interface MyNewsProps {
 
 
 
-const MyNews = ({searched}: MyNewsProps) => {
+const MyNews = ({ searched }: MyNewsProps) => {
     const [news, setNews] = useState<Welcome[]>([])
+    const [isLoading, setIsLoading] = useState(false)
     const fetchNews = async () => {
         const Url = 'https://api.spaceflightnewsapi.net/v4/articles'
         try {
@@ -18,6 +19,9 @@ const MyNews = ({searched}: MyNewsProps) => {
             if (response.ok) {
                 const data = await response.json()
                 setNews(data.results)
+                setTimeout(()=>{
+                    setIsLoading(false)
+                }, 1000)
             } else {
                 throw new Error('errore')
             }
@@ -28,18 +32,24 @@ const MyNews = ({searched}: MyNewsProps) => {
 
     }
     useEffect(() => {
+        setIsLoading(true)
         fetchNews()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searched])
     return (
         <Container className="my-5">
             <Row className=" justify-content-center gy-3">
-
-                {
+                {isLoading ? (
+                    <Col>
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </Col>
+                ) :
                     news.map(n => {
                         return (
                             <Col xs={12} key={n.id}>
-                                <SingleNews single={n}/>
+                                <SingleNews single={n} />
                             </Col>
                         )
                     })
